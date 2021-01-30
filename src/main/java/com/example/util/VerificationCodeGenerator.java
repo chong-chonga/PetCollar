2c6b726d4b35e09d93e7517d71af886a44da3eb9
乐鑫 黄 <1612682622@qq.com>
2021-01-30 20:08:20 +0800

    增设了 VerificationCodeGenerator 类, 用于生成指定长度的验证码

---
+++ src/main/java/com/example/util/VerificationCodeGenerator.java
@@ -0,0 +1,22 @@
+package com.example.util;
+
+import java.util.Random;
+
+/**
+ * @author Lexin Huang
+ */
+public class VerificationCodeGenerator {
+    private static final String CODE_STRING =
+            "123456789abcdefghijklmnopqrstxyzABCDEFGHIJKLMNOPQRSTXYZ";
+
+    private static Random random = new Random();
+
+
+    public static String getVerificationCode(Integer digits){
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < digits; i++) {
+            stringBuilder.append(CODE_STRING.charAt(random.nextInt(CODE_STRING.length())));
+        }
+        return stringBuilder.toString();
+    }
+}
