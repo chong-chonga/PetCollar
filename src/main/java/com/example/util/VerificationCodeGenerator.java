a1cb1c559a462b24ce7fadb9dec94c288bae0e1a
乐鑫 黄 <1612682622@qq.com>
2021-01-31 22:29:45 +0800

    修改了导入包的问题

--- src/main/java/com/example/util/VerificationCodeGenerator.java
+++ src/main/java/com/example/util/VerificationCodeGenerator.java
@@ -9,10 +9,10 @@ public class VerificationCodeGenerator {
     private static final String CODE_STRING =
             "123456789abcdefghijklmnopqrstxyzABCDEFGHIJKLMNOPQRSTXYZ";
 
-    private static Random random = new Random();
+    private static final Random random = new Random();
 
 
-    public static String getVerificationCode(Integer digits){
+    public static String generate(Integer digits){
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < digits; i++) {
             stringBuilder.append(CODE_STRING.charAt(random.nextInt(CODE_STRING.length())));
