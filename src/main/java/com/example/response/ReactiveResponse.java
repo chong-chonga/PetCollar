c5a0e26fa865060dd8ce02989b6dba1d1177ed09
乐鑫 黄 <1612682622@qq.com>
2021-01-31 14:35:57 +0800

    删除子类 StatusCode 中的部分状态码

--- src/main/java/com/example/response/ReactiveResponse.java
+++ src/main/java/com/example/response/ReactiveResponse.java
@@ -10,7 +10,7 @@ import static com.example.response.ReactiveResponse.StatusCode.*;
  * @since 2.0
  * 响应式消息的基类, 对于响应状态 code 和 msg 做出相应的调整
  * 对于 code 的设置, 将会自动匹配对应的 msg 且不允许手动设置 msg 的值
- * 只保留 data 的自定义能力, 且限制类型为 ReactiveData 如{@link LoginRegisterData}
+ * 只保留 data 的自定义能力, 且限制类型为 ReactiveData 如{@link AccountRequestData}
  * 将data设置为保护类型, 使得子类可以直接赋值, 而不允许外界调用
  */
 @ToString
@@ -25,28 +25,22 @@ public class ReactiveResponse {
     }
 
     public static class StatusCode {
+
         public static final int CORRECT = 200;
 
-        public static final int USERNAME_NOT_REGISTERED = 101;
+        public static final int USERNAME_FORMAT_WRONG = 411;
 
-        public static final int MISMATCH = 102;
+        public static final int PASSWORD_FORMAT_WRONG = 412;
 
-        @Deprecated
-        public static final int USERNAME_HAS_REGISTERED = 103;
+        public static final int MISMATCH = 413;
 
-        public static final int USERNAME_FORMAT_WRONG = 104;
+        public static final int USERNAME_HAS_REGISTERED = 414;
 
-        public static final int PASSWORD_FORMAT_WRONG = 105;
+        public static final int NEW_PASSWORD_FORMAT_WRONG = 415;
 
-        public static final int NOT_THE_ORIGINAL_PASSWORD = 106;
-
-        public static final int TOKEN_NOT_EXISTS = 151;
-
-        public static final int UNSUPPORTED_MAILBOX_TYPE = 410;
+        public static final int EMAIL_ADDRESS_NOT_SUPPORTED = 416;
 
         public static final int Server_ERROR = 500;
-
-        public static final int EMAIL_FUNCTION_NOT_AVAILABLE = 501;
 
         /**
          * @param statusCode 设置的状态码
@@ -56,26 +50,20 @@ public class ReactiveResponse {
             switch (statusCode) {
                 case CORRECT:
                     return "请求成功!";
-                case USERNAME_NOT_REGISTERED:
-                    return "该用户名尚未注册!";
-                case MISMATCH:
-                    return "用户名或密码错误!";
-                case USERNAME_HAS_REGISTERED:
-                    return "用户名已被注册!";
                 case USERNAME_FORMAT_WRONG:
                     return "用户名格式错误!";
                 case PASSWORD_FORMAT_WRONG:
                     return "密码格式错误!";
+                case MISMATCH:
+                    return "用户名或密码错误!";
+                case USERNAME_HAS_REGISTERED:
+                    return "用户名已被注册!";
+                case NEW_PASSWORD_FORMAT_WRONG:
+                    return "新密码格式错误!";
+                case EMAIL_ADDRESS_NOT_SUPPORTED:
+                    return "不支持的邮箱类型!";
                 case Server_ERROR:
                     return "服务器控制器错误!";
-                case NOT_THE_ORIGINAL_PASSWORD:
-                    return "输入密码与原密码不一致";
-                case TOKEN_NOT_EXISTS:
-                    return "无效令牌!";
-                case UNSUPPORTED_MAILBOX_TYPE:
-                    return "不支持的邮箱验证类型";
-                case EMAIL_FUNCTION_NOT_AVAILABLE:
-                    return "邮箱功能暂时不可用!";
                 default:
                     return "NONE";
             }
@@ -94,7 +82,7 @@ public class ReactiveResponse {
         return data;
     }
 
-    public void setContent(Integer code, ReactiveData<?> reactiveData){
+    public void setContent(Integer code, ReactiveData reactiveData){
         setStatus(code);
         this.data = reactiveData;
     }
