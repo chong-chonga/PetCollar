b826e80ad501954a6239b72810d13a871b94b960
乐鑫 黄 <1612682622@qq.com>
2021-01-31 13:33:14 +0800

    修改 getAccountVerificationResponse 方法的请求参数为单个参数, 即 AccountRequest类型的 request

--- src/main/java/com/example/service/UserService.java
+++ src/main/java/com/example/service/UserService.java
@@ -4,7 +4,6 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.example.pojo.AccountVerificationLevel;
 import com.example.pojo.User;
 import com.example.requrest.AccountRequest;
-import com.example.requrest.LoginRegisterRequest;
 import com.example.response.ReactiveResponse;
 
 /**
@@ -13,6 +12,5 @@ import com.example.response.ReactiveResponse;
 
 public interface UserService extends IService<User>{
 
-    ReactiveResponse getAccountVerificationResponse(AccountRequest request,
-                                                    AccountVerificationLevel level);
+    ReactiveResponse getAccountVerificationResponse(AccountRequest request);
 }
