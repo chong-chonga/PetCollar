664a3fcb3679572665c9aed85c3c620625f38723
乐鑫 黄 <1612682622@qq.com>
2021-02-01 20:08:28 +0800

    增加用于测试邮件服务的测试类

---
+++ src/test/java/com/example/MailServiceTest.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import com.example.pojo.User;
+import com.example.service.MailService;
+import lombok.extern.slf4j.Slf4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.mail.MessagingException;
+
+
+@Slf4j
+@SpringBootTest
+class MailServiceTest {
+    @Autowired
+    MailService mailService;
+
+
+    @Test
+    public void test1() throws MessagingException {
+        User user = new User();
+        user.setUsername("用户1");
+        user.setEmailAddress("1612682622@qq.com");
+        mailService.sendVerificationCodeMail(user, "WE91xq", 1L);
+    }
+}
