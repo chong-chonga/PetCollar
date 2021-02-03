0ba9d2f0ee693deaed8000cadd9f3f41d7160474
乐鑫 黄 <1612682622@qq.com>
2021-02-03 16:31:17 +0800

    自定义RedisTemplate<String,User>, 方便存储 User 对象

---
+++ src/main/java/com/example/config/RedisConfig.java
@@ -0,0 +1,26 @@
+package com.example.config;
+
+import com.example.pojo.User;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+
+/**
+ * @author Lexin Huang
+ */
+@Configuration
+public class RedisConfig {
+
+    @Bean(name = "userRedisTemplate")
+    public RedisTemplate<String, User> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, User> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        Jackson2JsonRedisSerializer<User> serializer =
+                new Jackson2JsonRedisSerializer<>(User.class);
+        template.setDefaultSerializer(serializer);
+        return template;
+    }
+
+}
