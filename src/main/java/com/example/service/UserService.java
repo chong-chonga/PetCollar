222eff8f176ce62c62aed0593579f6d7f71c9652
乐鑫 黄 <1612682622@qq.com>
2021-02-05 19:42:46 +0800

    新增接口方法 updatePassword 的默认实现, 并在两个实现类中调用该方法; 并将原接口方法 getOperationResponse 名称更改为 getSettingsResponse

--- src/main/java/com/example/service/UserService.java
+++ src/main/java/com/example/service/UserService.java
@@ -1,9 +1,10 @@
 package com.example.service;
 
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.example.pojo.User;
 import com.example.request.AccountVerificationRequest;
-import com.example.request.OperationRequest;
+import com.example.request.AccountSettingsRequest;
 import com.example.response.ReactiveData;
 import com.example.response.ReactiveResponse;
 
@@ -19,10 +20,17 @@ public interface UserService extends IService<User>{
         return response;
     }
 
-    default ReactiveResponse getOperationResponse(OperationRequest request){
+    default ReactiveResponse getSettingsResponse(AccountSettingsRequest request){
         ReactiveResponse response = new ReactiveResponse();
         response.setContent(ReactiveResponse.StatusCode.Server_ERROR, new ReactiveData());
         return response;
+    }
+
+    default void updatePassword(String username, String newPassword){
+        UpdateWrapper<User> updateWrapper = new UpdateWrapper<>();
+        updateWrapper.eq("user_username", username);
+        updateWrapper.set("user_password", newPassword);
+        update(updateWrapper);
     }
 
 
