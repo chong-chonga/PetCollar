e48e1647405cbc799796c66243b0029b1ca4a34e
乐鑫 黄 <1612682622@qq.com>
2021-02-23 19:25:05 +0800

    更新了请求枚举属性的名称

--- src/main/java/com/example/request/PetRequestType.java
+++ src/main/java/com/example/request/PetRequestType.java
@@ -4,5 +4,5 @@ package com.example.request;
  * @author Lexin Huang
  */
 public enum PetRequestType {
-    SEARCH_USER_PETS, ADD_PET, GET_USER_PETS, MODIFY_PET_INTRODUCTION
+    SEARCH_USER_PETS, ADD_PET, GET_USER_PETS, MODIFY_PET_INTRODUCTION, DELETE_PET
 }
