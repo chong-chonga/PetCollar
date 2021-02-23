bc220780b9a48c2fbd75e9b624107b906a60b091
乐鑫 黄 <1612682622@qq.com>
2021-02-23 19:15:32 +0800

    添加了用于请求参数校验的接口

---
+++ src/main/java/com/example/service/BasicRequestInfoFormatService.java
@@ -0,0 +1,12 @@
+package com.example.service;
+
+/**
+ * @author Lexin Huang
+ * @since 3.0
+ */
+public interface BasicRequestInfoFormatService {
+    Boolean isNameFormatCorrect(String testName);
+
+    Boolean isTextFormatCorrect(String testText);
+
+}
