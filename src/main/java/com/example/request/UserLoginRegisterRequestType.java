d358e476e1672e3569a3f1d24acdad241395df29
乐鑫 黄 <1612682622@qq.com>
2021-02-23 19:22:59 +0800

    重命名为 UserLoginRegister 为前缀

---
+++ src/main/java/com/example/request/UserLoginRegisterRequestType.java
@@ -0,0 +1,8 @@
+package com.example.request;
+
+/**
+ * @author Lexin Huang
+ */
+public enum UserLoginRegisterRequestType {
+    NORMAL_LOGIN, TOKEN_LOGIN, REGISTER, RETRIEVE_PASSWORD, SUBMIT_RESET
+}
