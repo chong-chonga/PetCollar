f643809bb910c360ce7d44cad971c584460c4fef
乐鑫 黄 <1612682622@qq.com>
2021-03-06 22:03:28 +0800

    更新用于接收参数的 Request 类, 统一使用注解进行校验

--- src/main/java/com/example/request/UserSettingsRequest.java
+++ src/main/java/com/example/request/UserSettingsRequest.java
@@ -1,25 +1,52 @@
 package com.example.request;
 
-import lombok.AllArgsConstructor;
+import com.example.pojo.UserProfile;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Pattern;
 
 /**
  * @author Lexin Huang
  */
 @Data
-@ToString
-@AllArgsConstructor
-@NoArgsConstructor
+@ApiModel(value = "用户个人设置请求信息")
 public class UserSettingsRequest {
 
+    @Pattern(regexp = "^[\\u4e00-\\u9fa5a-zA-Z0-9]{4,16}$",
+            message = "用户名必须在 4-16个字符以内，支持中英文、数字", groups = {UpdateTransaction.class})
+    @ApiModelProperty(value = "用户名")
     private String username;
 
+    @Pattern(regexp = "^([A-Z]|[a-z]|[0-9]|[~!@#$%^&*()+=|{}':;,\\\\.<>/\\-_?]){6,18}$",
+            message = "密码必须在 6-18 位，支持字母、数字、特殊字符",
+            groups = {VerificationTransaction.class})
+    @ApiModelProperty(value = "密码")
     private String oldPassword;
 
+    @Pattern(regexp = "^([A-Z]|[a-z]|[0-9]|[~!@#$%^&*()+=|{}':;,\\\\.<>/\\-_?]){6,18}$",
+            message = "密码必须在 6-18 位，支持字母、数字、特殊字符",
+            groups = {VerificationTransaction.class})
+    @ApiModelProperty(value = "密码")
     private String newPassword;
 
-    private String introduction;
+    @Length(max = 255, message = "个人介绍必须在 255 个字符以内!", groups = {UpdateTransaction.class})
+    @ApiModelProperty(value = "用户个人介绍; 可以为空字符串, 不可以为 null !")
+    private String userIntroduction;
+
+    @Pattern(regexp = "[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\\.[a-zA-Z0-9_-]+)+$", message = "请输入正确的邮箱!"
+            , groups = {UpdateTransaction.class})
+    @ApiModelProperty(value = "用户邮箱")
+    private String emailAddress;
+
+    public UserProfile createUserProfile() {
+        UserProfile userProfile = new UserProfile();
+        userProfile.setUsername(this.username);
+        userProfile.setUserIntroduction(this.userIntroduction);
+        userProfile.setEmailAddress(this.emailAddress);
+        return userProfile;
+    }
 
 }
