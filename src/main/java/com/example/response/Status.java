7f70e224f3d839e55c7f1141b08e472ed464f5d7
乐鑫 黄 <1612682622@qq.com>
2021-03-09 19:54:48 +0800

    删除用于提示参数格式错误的枚举属性

--- src/main/java/com/example/response/Status.java
+++ src/main/java/com/example/response/Status.java
@@ -6,36 +6,31 @@ package com.example.response;
 public enum Status {
     //      ---SUCCESS---
     SUCCESS(200, "请求成功"),
+    //      ---Not Found---
+    RESOURCE_NOT_FOUND(404, "找不到指定资源"),
 
 
     //      ---WEB_ERROR---
-    USERNAME_FORMAT_WRONG(100001, "用户名格式有误! 用户名限4-16个字符，支持中英文、数字"),
-    PASSWORD_FORMAT_WRONG(100002, "密码格式有误! 密码限6-18 位，支持字母、数字,特殊字符"),
-    EMAIL_FORMAT_WRONG(100003, "邮箱格式有误! 限QQ邮箱和网易邮箱, 且邮箱长度不得超过31个字符!"),
-    USERNAME_NOT_AVAILABLE(100004, "该名称已被使用过了, 请再挑个名称试试!"),
-    MISMATCH(100005, "用户名或密码错误!"),
-    VERIFICATION_CODE_WRONG(100006, "验证码错误!"),
+    USERNAME_NOT_AVAILABLE(100001, "该名称已被使用过了, 请再挑个名称试试!"),
+    MISMATCH(100002, "用户名或密码错误!"),
+    VERIFICATION_CODE_WRONG(100003, "验证码错误!"),
 
 
     //      ---SERVICE_ERROR---
     INVALID_TOKEN(200001, "令牌无效, 请重新登录!"),
     UNAUTHORIZED(200002, "您没有访问这个资源的权限!"),
-    INVALID_ITEM(200003, "您还没有这个物品! 请获取后再进行尝试!"),
 
     //        USER_MODULE
     USER_PASSWORD_WRONG(300001, "密码错误! 请检查是否为原密码!"),
-    TEXT_FORMAT_WRONG(300002, "个人介绍必须在 255 个字符内!"),
 
     //      ---PARAM_ERROR---
 //        PET_MODULE
-    PET_NAME_FORMAT_WRONG(400001, "宠物名称必须在 4-16 个字符内, 由中英文,数字组成!"),
-    PET_NAME_NOT_AVAILABLE(400002, "该宠物名称已经被用了, 换一个试试~"),
-    PET_INTRODUCTION_TOO_LONG(400003, "宠物介绍不能超过 255 个字符!"),
+    PET_NAME_NOT_AVAILABLE(400001, "该宠物名称已经被用了, 换一个试试~"),
 
 
     //       ---SYS_ERROR---
     MAIL_SERVICE_NOT_AVAILABLE(900001, "邮箱服务暂时不可用!"),
-    RESOURCE_NOT_FOUND(404, "找不到指定资源"),
+    AVATAR_SERVICE_NOT_AVAILABLE(900002, "头像服务暂时不可用!"),
     SERVER_ERROR(999999, "服务器出错了! 请联系管理员以获得帮助");
 
 
