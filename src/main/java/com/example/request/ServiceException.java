ad4590299571baa2acb4fe2251f0d31636cad53d
乐鑫 黄 <1612682622@qq.com>
2021-03-09 19:55:25 +0800

    增加用于表示业务异常的基类

---
+++ src/main/java/com/example/request/ServiceException.java
@@ -0,0 +1,10 @@
+package com.example.request;
+
+/**
+ * @author Lexin Huang
+ */
+public class ServiceException extends RuntimeException{
+    public ServiceException(String msg){
+        super(msg);
+    }
+}
